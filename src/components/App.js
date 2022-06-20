1febfca72d993afdb57af9e31dec90cf96731f18
seo0yoon <genious921225@gmail.com>
2022-06-21 01:44:34 +0900

    임시저장

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,40 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import PlayList from "./PlayList";
+import mockItems from "../mock.json";
+import { getPlayList } from "../api";
 
 const App = () => {
-  return <div>App</div>;
+  const [items, setItems] = useState(mockItems);
+  const [order, setOrder] = useState("createdAt");
+  const sortedItems = items.sort((a, b) => b[order] - a[order]);
+
+  const hadleTitleClick = () => setOrder("createdAt");
+  const hadleBestClick = () => setOrder("rating");
+
+  const handleDelete = (id) => {
+    const nextItems = items.filter((item) => item.id !== id);
+    setItems(nextItems);
+  };
+
+  const handleLoadClick = async (options) => {
+    await getPlayList(options);
+    setItems();
+  };
+
+  useEffect(() => {
+    handleLoadClick({ order, offset: 0, limit: 6 });
+  }, [order]);
+
+  return (
+    <div>
+      <div>
+        <button onClick={hadleTitleClick}>제목순</button>
+        <button onClick={hadleBestClick}>베스트순</button>
+      </div>
+      <PlayList items={sortedItems} onDelete={handleDelete} />
+      <button onClick={handleLoadClick}>더보기</button>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/PlayList.js
@@ -0,0 +1,48 @@
+import React from "react";
+import styled from "styled-components";
+
+const PlayListItem = ({ item, onDelete }) => {
+  const handleDeleteClick = () => onDelete(item.id);
+
+  return (
+    <PlayListItemWrap>
+      <PlayListImg src={item.img} alt={item.artist} />
+      <div>
+        <p>{item.artist}</p>
+        <h1>{item.title}</h1>
+        <p>{item.rating}</p>
+        <p>{item.content}</p>
+        <button onClick={handleDeleteClick}>삭제</button>
+      </div>
+    </PlayListItemWrap>
+  );
+};
+
+const PlayList = ({ items, onDelete }) => {
+  return (
+    <ul>
+      {items.map((item) => {
+        return (
+          <li key={item.id}>
+            <PlayListItem item={item} onDelete={onDelete} />
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default PlayList;
+
+const PlayListItemWrap = styled.div`
+  display: flex;
+  padding: 10px;
+  align-items: center;
+`;
+
+const PlayListImg = styled.img`
+  width: 200px;
+  height: 200px;
+  object-fit: cover;
+  margin-right: 20px;
+`;
